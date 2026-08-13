392c8b47ab37c97d1b7f0e502033eeaf9287a82c
sav <xaviothomas@gmail.com>
2026-08-13 13:42:13 +0530

    palindrome failure

--- 4palcheck.c
+++ 4palcheck.c
@@ -9,17 +9,28 @@ int ispal (char str[]){
   int l=0,r=strlen(str)-1;
   while(r>l){
     if(str[l]!=str[r]){return 0;}
-    r++; l++;
+    r--; l++;
   }
   return 1;
 }
 
 int main(){
   
-  char str[3][5]={"12456","86524","12321"};
-  for(int i=0;i<3;i++){
-    if(ispal(str[i])){printf("\n%s is palindrome",str[i]);}
-    else{printf("\n%s is not a palindrome",str[i]);}
+  char str[10][10];
+  int lim;
+  printf("\nEnter the number of strings(max limit=10) :");
+  scanf("%d",&lim);
+
+  for(int i=0;i<lim;i++){
+    printf("\nEnter string %d (char limit=10):",i);
+    scanf("%s",&str[i]);
+  }
+
+  for(int i=0;i<lim;i++){
+    if(ispal(str[i])){
+      printf("\n%s is palindrome",str[i]);}
+    else{
+      printf("\n%s is not a palindrome",str[i]);}
   }
   return 0;
 }
