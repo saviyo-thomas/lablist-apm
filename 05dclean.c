4175da715e7a34db3669a5360e28e533e9d9c685
sav <xaviothomas@gmail.com>
2026-09-05 11:10:43 +0530

    blurt

--- 05dclean.c
+++ 05dclean.c
@@ -5,23 +5,23 @@
 char cmail[LIM][50];
 
 void check(int s){
- for (int i=0;i<s;i++){
+ for (int i=0;i<s-1;i++){
    for (int j=0;j<s;j++){
-     if (strcmp(cmail)
- }}
-}
+     if (strcmp(cmail[i],cmail[j])==0){ cmail[j][0]='\0';}
+}}}
 int main(){
   int lim;
   printf("\n(Max limit=%d)\nEnter number of emails :",LIM);
-  scanf("%d",&lim);
+  if(scanf("%d",&lim)!=1||lim>LIM){return 1;}
+  getchar(); //!!buffer clearup
   for(int i=0;i<=lim;i++){
     fgets(cmail[i],50,stdin);
+    cmail[i][strcspn(cmail[i], "\n")]='\0';
   }
 
   check(lim);
 
+  printf("\nUnique Emails:\n");
   for(int i=0;i<=lim;i++){
-    printf("%s",cmail[i]);
-  }
-  return 0;
-}
+    if (cmail[i][0]!='\0'){ printf("%s\n",cmail[i]);}
+} return 0;}
