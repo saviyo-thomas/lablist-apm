94f1b8957169a946694ddd810500d2e815406cd9
Saviyo Thomas <64469898+saviyo-thomas@users.noreply.github.com>
2026-09-11 10:38:14 +0530

    Update 05dclean.c

--- 05dclean.c
+++ 05dclean.c
@@ -1,27 +1,35 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 #define LIM 50
-
 char cmail[LIM][50];
-
-void check(int s){
- for (int i=0;i<s-1;i++){
-   for (int j=0;j<s;j++){
-     if (strcmp(cmail[i],cmail[j])==0){ cmail[j][0]='\0';}
-}}}
-int main(){
-  int lim;
-  printf("\n(Max limit=%d)\nEnter number of emails :",LIM);
-  if(scanf("%d",&lim)!=1||lim>LIM){return 1;}
-  getchar(); //!!buffer clearup
-  for(int i=0;i<=lim;i++){
-    fgets(cmail[i],50,stdin);
-    cmail[i][strcspn(cmail[i], "\n")]='\0';
-  }
-
-  check(lim);
-
-  printf("\nUnique Emails:\n");
-  for(int i=0;i<=lim;i++){
-    if (cmail[i][0]!='\0'){ printf("%s\n",cmail[i]);}
-} return 0;}
+void check(int s) {
+for (int i = 0; i < s; i++) {
+if (cmail[i][0] == '\0') continue;
+for (int j = i + 1; j < s; j++) {
+if (strcmp(cmail[i], cmail[j]) == 0) {
+cmail[j][0] = '\0';
+}
+}
+}
+}
+int main() {
+int lim;
+printf("\n(Max limit=%d)\nEnter number of emails: ", LIM);
+if (scanf("%d", &lim) != 1 || lim > LIM || lim <= 0) {
+return 1;
+}
+getchar();
+for (int i = 0; i < lim; i++) {
+if (fgets(cmail[i], 50, stdin) != NULL) {
+cmail[i][strcspn(cmail[i], "\n")] = '\0';
+}
+}
+check(lim);
+printf("\nUnique Emails:\n");
+for (int i = 0; i < lim; i++) {
+if (cmail[i][0] != '\0') {
+printf("%s\n", cmail[i]);
+}
+}
+return 0;
+}
