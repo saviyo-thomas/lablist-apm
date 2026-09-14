e13719065cc4691da6548f2195e2970c5aa4d85b
saviyo-thomas <saviyothomas@gmail.com>
2026-09-14 12:53:44 +0000

    11

--- 09symmetrycheck.c
+++ 09symmetrycheck.c
@@ -1,27 +1,47 @@
 //Program to check if given matrix is symmetric or not
-#include<stdio.h>
+#include <stdio.h>
 #define m 5
 
-int a[m][m];
-
-void gtel(int t[m][m]){
- printf("\nEnter elements separated by space\n");
- for(int i=0;i<m;i++){ for(int j=0;j<m;j++){
-   scanf("%d",&t[i][j]);
-  }}
-  return;
+void gtel(int t[m][m]) {
+    printf("Enter elements separated by space:\n");
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < m; j++) {
+            scanf("%d", &t[i][j]);
+        }
+    }
 }
 
-void check(int t[m][m]){
-    for (int i=0;i<m;i++){ for (int j=0;j<mj++){
-        if(t[i][j]!=t[j][i]){return;}
-        printf("\n the matrix is symmetric");
-    }}
+int check(int t[m][m]) {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < m; j++) {
+            if (t[i][j] != t[j][i]) {
+                return 0; // Not symmetric
+            }
+        }
+    }
+    return 1; // Symmetric
 }
 
-int main(){
-    int a[MAXZISE],[MAXZISE];
-    gtel();
-    check();
+int main() {
+    int a[m][m];
+    gtel(a);
+    
+    if (check(a)) {
+        printf("\nThe matrix is symmetric\n");
+    } else {
+        printf("\nThe matrix is not symmetric\n");
+    }
+    
     return 0;
 }
+
+/*
+Enter elements separated by space:
+1  2  3  4  5
+2  6  7  8  9
+3  7 10 11 12
+4  8 11 13 14
+5  9 12 14 15
+
+The matrix is symmetric
+*/
